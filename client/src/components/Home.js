8a4062e7cb64eb023d525c992c5783682bdba6e9
David Belnap <david.belnap6@gmail.com>
2017-10-06 10:55:51 -0600

    axios added

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Image, Segment } from 'semantic-ui-react';
+import star_wars_logo from '../images/star_wars_logo.png';
+import axios from 'axios';
 
 class Home extends Component {
+	state = { planets: '' };
+
+	componentDidMount() {
+		axios.get('/api/planets/')
+			.then(res => {
+				this.setState({ planets: res.data.data })
+			})
+			.catch( error => {
+				console.log(error.response);
+			})
+	}
+
+	displayPlanets() {
+		let { planet } = this.state;
+	}
+
   render() {
     return (
-      <Header as='h1' textAlign='center'>Home Component</Header>
+    	<Segment basic inverted>
+	    	<Segment basic inverted textAlign='center'>
+	    		<Image centered size='small' src={star_wars_logo} alt='Star Wars Logo' />
+	    	</Segment>
+	    	<Segment basic inverted>
+	      	<Header as='h1' textAlign='center'>Planets</Header>
+	      	<displayPlanets planets={this.state.planets} />
+	    	</Segment>
+    	</Segment>
     );
   }
 }
+
 
 export default Home;
